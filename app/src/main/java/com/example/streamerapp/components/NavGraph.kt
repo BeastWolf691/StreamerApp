269cb4dc4b53d4b783126aca79f0413e5591b73b
Steven Riviere <sriver@hotmail.fr>
2024-06-27 11:11:02 +0200

    màj des differents fichiers card

--- app/src/main/java/com/example/streamerapp/components/NavGraph.kt
+++
@@ -1,9 +0,0 @@
-package com.example.streamerapp.components
-
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.runtime.Composable
-
-@Composable
-fun NavGraph(innerPaddingValues: PaddingValues) {
-
-}
